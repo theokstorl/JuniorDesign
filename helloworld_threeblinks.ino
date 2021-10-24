2e6d91fccf6a94e761c467ccdc19f1c8e3a2361a
theokstorldesmond <50800368+theokstorldesmond@users.noreply.github.com>
2021-10-24 17:53:47 -0400

    Create helloworld_threeblinks.ino
    makes three LEDS blink sequentially.

---
+++ helloworld_threeblinks.ino
@@ -0,0 +1,26 @@
+void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(9, INPUT);
+}
+
+void loop() {
+  if(digitalRead(9) == HIGH){
+    digitalWrite(8, HIGH);
+    delay(900);
+    digitalWrite(8, LOW);
+    delay(900);
+
+    digitalWrite(12, HIGH);
+    delay(900);
+    digitalWrite(12, LOW);
+    delay(1000);
+
+    digitalWrite(13, HIGH);
+    delay(900);
+    digitalWrite(13, LOW);
+    delay(900);
+    
+  }
+}
