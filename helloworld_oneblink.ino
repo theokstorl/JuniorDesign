6479217839facd46201ec85769ac8b07c0450bd1
theokstorldesmond <50800368+theokstorldesmond@users.noreply.github.com>
2021-10-24 17:52:56 -0400

    Create helloworld_oneblink.ino
    makes one LED blink

---
+++ helloworld_oneblink.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(9, INPUT);
+}
+
+void loop() {
+  if(digitalRead(9) == HIGH){
+    digitalWrite(8, HIGH);
+    delay(900);
+    digitalWrite(8, LOW);
+    delay(900);
+  }
+}
